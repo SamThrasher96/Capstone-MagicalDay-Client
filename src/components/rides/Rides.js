8aab697bcd66e2033d41dbc11a811fbbab65f1ed
Sam Thrasher <samthrasher9@gmail.com>
2023-09-22 00:46:21 -0500

    styled all rides page, all shows page, and all restaurants page

--- src/components/rides/Rides.js
+++ src/components/rides/Rides.js
@@ -1,18 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getAllRideDetails } from "../../managers/rideManager";
+import { Button, TextField } from "@mui/material";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
 import "./ride.css";
 
 export const AllRides = () => {
     const [rides, setRides] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [showScrollButton, setShowScrollButton] = useState(false); 
     const navigate = useNavigate();
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth", 
+        });
+    };
 
     useEffect(() => {
         getAllRideDetails().then((data) => {
             setRides(data);
         });
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
     }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 100) {
+            setShowScrollButton(true);
+        } else {
+            setShowScrollButton(false);
+        }
+    };
 
     const filteredRides = rides.filter((ride) =>
         ride.ride_name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -20,12 +44,13 @@ export const AllRides = () => {
 
     return (
         <div className="rides-container">
-            <h2>All Rides</h2>
-            <input
-                type="text"
-                placeholder="Search for rides..."
+            <h2 className="ride-name">All Rides</h2>
+            <TextField
+                label="Search for rides..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                variant="outlined" 
+                fullWidth 
                 className="search-input"
             />
             <div className="rides-list">
@@ -42,6 +67,29 @@ export const AllRides = () => {
                     </div>
                 ))}
             </div>
+
+            {showScrollButton && (
+                <Button
+                    className="scroll-to-top"
+                    onClick={scrollToTop}
+                    style={{
+                        position: "fixed",
+                        bottom: "40px",
+                        right: "40px",
+                        color: "#fff",
+                        borderRadius: "8px", 
+                        width: "50px", 
+                        height: "50px", 
+                        zIndex: "99", 
+                    }}
+                    startIcon={
+                        <KeyboardDoubleArrowUpIcon
+                            style={{ fontSize: 50 }} 
+                        />
+                    } 
+                >
+                </Button>
+            )}
         </div>
     );
 };
